92d2116ad719a705a2792cbb1d648a142ea4e1d1
Chris <147110876+Kl51Chris@users.noreply.github.com>
2025-12-08 13:53:48 -0600

    add in Log of TO result

--- run_forced_dynamics.m
+++ run_forced_dynamics.m
@@ -38,9 +38,19 @@ N_to   = numel(tspan) - 1;
 y0=[xt0;yt0];
 ref_to = make_straight_ref(tspan, y0);
 save('log.mat','ref_to'); 
-
-[x_to, u_to, to_dbg] = towing_trajopt(dt_to, N_to, ref_to, params.x0, params); %TO
-save('TO_Output','x_to', 'u_to', 'to_dbg');
+%% Get Past TO result to reuse Warning: delete TO_Output.mat for new ref path
+if isfile('TO_Output.mat')
+    % load cached TO result
+    S = load('TO_Output.mat', 'x_to', 'u_to', 'to_dbg');
+    x_to   = S.x_to;
+    u_to   = S.u_to;
+    to_dbg = S.to_dbg;
+else
+    % run TO and save for next time
+    [x_to, u_to, to_dbg] = towing_trajopt(dt_to, N_to, ref_to, params.x0, params);
+    save('TO_Output.mat', 'x_to', 'u_to', 'to_dbg');
+end
+%%
 traj_to = build_to_qp_traj(dt_to, x_to, u_to, params);
 params.to_traj = traj_to;
 controller = @(x,t) qp_to_wrapper(x, t, params);
