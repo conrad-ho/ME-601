f8cc83c9fa4ef0c4786dcdcb0dc9f1fe886d1bfb
Chris <147110876+Kl51Chris@users.noreply.github.com>
2025-12-08 21:54:59 -0600

    Baumgarte update

--- towing_dynamics_full.m
+++ towing_dynamics_full.m
@@ -1,26 +1,87 @@
 function xdot = towing_dynamics_full(x, u, params)
-    import casadi.*
+import casadi.*
+%TOWING_DYNAMICS_FULL
+%   Continuous-time rigid robot+trailer dynamics with Baumgarte-stabilized
+%   holonomic (hitch) + nonholonomic constraints.
+%
+% State:
+%   x = [xr; yr; thetar; vxr; vyr; wr;
+%        xt; yt; thetat; vxt; vyt; wt]  (12x1)
+%
+% Input:
+%   u = [F_drive; tau_r]
+%
+% Output:
+%   xdot, same ordering as x: [vxr; vyr; wr; axr; ayr; alphar; ...]
 
-    % 拆 q,v
-    q = [x(1:3); x(7:9)];
-    v = [x(4:6); x(10:12)];
 
+    % Baumgarte gains
+    if ~isfield(params, 'alpha_baum'), params.alpha_baum = 5.0;  end;
+    if ~isfield(params, 'beta_baum'),  params.beta_baum  = 10.0; end;
+    alpha = params.alpha_baum;
+    beta  = params.beta_baum;
+
+    % unpack state
+    xr = x(1);  yr = x(2); thetar  = x(3);
+    vxr = x(4); vyr = x(5); wr     = x(6);
+    xt = x(7);  yt = x(8); thetat  = x(9);
+    vxt = x(10); vyt = x(11); wt   = x(12);
+
+    % get matrices
     dyn  = towing_dynamics_mats(x, params);
-    M    = dyn.M;      % 6x6
-    B    = dyn.B;      % 6x2
-    J    = dyn.J;      % 3x6
-    dotJ = dyn.dotJ;   % 3x6
-    vgen = dyn.v;      % 6x1
+    M    = dyn.M;        % 6x6
+    B    = dyn.B;        % 6x2
+    J    = dyn.J;        % 3x6
+    dotJ = dyn.dotJ;     % 3x6
+    vgen = dyn.v;        % 6x1
 
-    % KKT 系统
-    K   = [M, -J';
-           J, SX.zeros(size(J,1), size(J,1))];    % 9x9
+    % === position-level constraint h(q) ===
+    % same geometry as in towing_dynamics_mats
+    d  = params.d;
+    Lt = params.Lt;
+
+    r_rh_body = [-d;   0];
+    r_th_body = [ Lt/2; 0];
+
+    cr = cos(thetar); sr = sin(thetar);
+    ct = cos(thetat); st = sin(thetat);
+
+    Rr = [ cr, -sr;
+           sr,  cr];
+    Rt = [ ct, -st;
+           st,  ct];
+
+    p_r = Rr * r_rh_body;
+    p_t = Rt * r_th_body;
+
+    p_h_r  = [xr; yr] + p_r;
+    p_h_tr = [xt; yt] + p_t;
+
+    phi_pos = p_h_r - p_h_tr;    % 2x1 holonomic position error
+
+    % third constraint (nonholonomic) has no position level: h3 = 0
+    h = [phi_pos;
+         0];                     % 3x1
+
+    % === Baumgarte-stabilized constraint equation ===
+    %   J a + dotJ v + 2 alpha J v + beta^2 h = 0
+    Jv      = J * vgen;                           % 3x1
+    rhs_con = -dotJ * vgen - 2*alpha*Jv - (beta^2) * h;  % 3x1
+
+    % === KKT system ===
+    % [ M   J';   [a    = [ B*u
+    %   J   0 ]   lambda]   rhs_con ]
+    m_con = size(J,1);
+    K   = [M,  J.';
+           J,  zeros(m_con, m_con)];
+
     rhs = [B*u;
-          -dotJ*vgen];                            % 9x1
+           rhs_con];
 
     sol = K \ rhs;
-    a   = sol(1:6);   % generalized acceleration
+    a   = sol(1:6);      % generalized acceleration
 
+    % === assemble xdot ===
     qdot = vgen;
     vdot = a;
 
--- towing_dynamics_mats.m
+++ towing_dynamics_mats.m
@@ -1,65 +1,97 @@
 function dyn = towing_dynamics_mats(x, params)
-    % x: 12x1 [xr; yr; thetar; vxr; vyr; wr; xt; yt; thetat; vxt; vyt; wt]
+%TOWING_DYNAMICS_MATS
+%   Common robot+trailer matrices used by TO, simulation and QP.
+%
+% State:
+%   x = [xr; yr; thetar; vxr; vyr; wr;
+%        xt; yt; thetat; vxt; vyt; wt]  (12x1)
+%
+% Params must contain:
+%   m_r, I_r, m_t, I_t, d, Lt
 
-    xr = x(1); yr = x(2); thetar = x(3);
-    vxr = x(4); vyr = x(5); wr = x(6);
-    xt = x(7); yt = x(8); thetat = x(9);
-    vxt = x(10); vyt = x(11); wt = x(12);
+    % unpack state
+    xr = x(1);  yr = x(2); thetar  = x(3);
+    vxr = x(4); vyr = x(5); wr     = x(6);
+    xt = x(7);  yt = x(8); thetat  = x(9);
+    vxt = x(10); vyt = x(11); wt   = x(12);
 
+    % generalized velocity
     v = [vxr; vyr; wr; vxt; vyt; wt];
 
+    % params
     m_r = params.m_r; I_r = params.I_r;
     m_t = params.m_t; I_t = params.I_t;
     d   = params.d;   Lt  = params.Lt;
 
-    % 1) Mass Matrix
+    % === 1) Mass matrix (6x6) ===
     M = diag([m_r m_r I_r m_t m_t I_t]);
 
-    % 2) B
-    B = [ cos(thetar), 0;
-          sin(thetar), 0;
-          0,           1;
-          0,           0;
-          0,           0;
-          0,           0 ];
+    % === 2) Input matrix B (6x2) ===
+    % u = [F_drive; tau_r], F_drive is along robot body +x
+    cr = cos(thetar); sr = sin(thetar);
+    B = [ cr, 0;
+          sr, 0;
+          0,  1;
+          0,  0;
+          0,  0;
+          0,  0 ];
 
-    % 3) constraint
-    r_rh_body = [-d; 0];
-    r_th_body = [Lt/2; 0];
+    % === 3) Constraint Jacobian J (3x6) ===
+    % hitch offsets in body frames
+    r_rh_body = [-d;   0];   % robot COM -> hitch
+    r_th_body = [ Lt/2; 0];  % trailer COM -> hitch
 
-    Rr = [cos(thetar) -sin(thetar);
-          sin(thetar)  cos(thetar)];
-    Rt = [cos(thetat) -sin(thetat);
-          sin(thetat)  cos(thetat)];
+    % rotation matrices
+    ct = cos(thetat); st = sin(thetat);
+    Rr = [ cr, -sr;
+           sr,  cr];
+    Rt = [ ct, -st;
+           st,  ct];
 
-    p_r = Rr * r_rh_body;
-    p_t = Rt * r_th_body;
+    % hitch vectors in world frame
+    p_r = Rr * r_rh_body;   % 2x1
+    p_t = Rt * r_th_body;   % 2x1
 
+    % skew-like terms for angular velocity contribution
     S_p_r = [-p_r(2); p_r(1)];
     S_p_t = [-p_t(2); p_t(1)];
 
-    % 4) constraint Jacobian J (3x6)
-    J_holo = [ eye(2), S_p_r, -eye(2), -S_p_t ];  % hitch coincidence (2 rows)
-    l = [-sin(thetat); cos(thetat)];
-    J_nonholo = [0 0 0 l(1) l(2) 0];              % trailer lateral velocity = 0
-    J = [J_holo; J_nonholo];
+    % holonomic: hitch points coincide
+    % [vx_r; vy_r] + S_p_r*wr  - [vx_t; vy_t] - S_p_t*wt = 0
+    J_holo = [ eye(2),  S_p_r,  -eye(2), -S_p_t ];   % 2x6
 
-    % 5) dotJ
-    Sd_p_r = [ d*cos(thetar)*wr;            d*sin(thetar)*wr ];
-    Sd_p_t = [ -(Lt/2)*cos(thetat)*wt;      (Lt/2)*sin(thetat)*wt ];
+    % nonholonomic: trailer lateral velocity = 0 (body y-axis)
+    l = [-sin(thetat); cos(thetat)];                 % body y in world
+    J_nonholo = [0 0 0 l(1) l(2) 0];                 % 1x6
+
+    J = [J_holo;
+         J_nonholo];                                 % 3x6
+
+    % === 4) Constraint Jacobian time derivative dotJ (3x6) ===
+    % NOTE: keep same form as旧代码，避免引入新的几何差异
+    Sd_p_r = [ d*cos(thetar)*wr;
+               d*sin(thetar)*wr ];
+
+    Sd_p_t = [ -(Lt/2)*cos(thetat)*wt;
+                (Lt/2)*sin(thetat)*wt ];
 
     dotJ_holo = [ zeros(2,2), Sd_p_r, zeros(2,2), -Sd_p_t ];
-    ldot = [-cos(thetat)*wt; -sin(thetat)*wt];
-    dotJ_nonholo = [0 0 0 ldot(1) ldot(2) 0];
-    dotJ = [dotJ_holo; dotJ_nonholo];
 
-    % 6) J_y for hitch task (unchanged)
+    ldot = [-cos(thetat)*wt;
+            -sin(thetat)*wt];
+    dotJ_nonholo = [0 0 0 ldot(1) ldot(2) 0];
+
+    dotJ = [dotJ_holo;
+            dotJ_nonholo];
+
+    % === 5) Hitch task Jacobian J_y (2x6) and Jdot_y_v (2x1) ===
     J_y = [ 1, 0,  d*sin(thetar),  0, 0, 0;
             0, 1, -d*cos(thetar),  0, 0, 0 ];
+
     Jdot_y_v = [ d*cos(thetar)*wr;
                  d*sin(thetar)*wr ];
 
-    % wrapper to struct
+    % pack struct
     dyn.M = M;
     dyn.B = B;
     dyn.J = J;
@@ -67,5 +99,5 @@ function dyn = towing_dynamics_mats(x, params)
     dyn.v = v;
     dyn.J_y = J_y;
     dyn.Jdot_y_v = Jdot_y_v;
-    dyn.H = zeros(6,1);
+    dyn.H = zeros(6,1);  % placeholder if needed
 end
